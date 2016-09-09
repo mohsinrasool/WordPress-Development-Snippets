a06ef12f1cc39812b9a7517856b38282df9fcd3a
Mohsin Rasool <mohsin.rasool@gmail.com>
2016-09-09 16:47:10 +0500

    Dynamic Date: This plugin provides you a shortcodes [dynamic_date days_before="3"] that return the date which is "days_before" the current date.

---
+++ shortcodes/ms_dynamic_date/index.php
@@ -0,0 +1,270 @@
+<?php
+/**
+ * Plugin Name:       Dynamic Date
+ * Description:       This plugin provides you a shortcodes [dynamic_date days_before="3"] that return the date which is "days_before" the current date.
+ * Version:           1.0
+ * Author:            Meticulous Solutions
+ * Author URI:        http://MeticulouSolutions.com
+ * License:           GPL2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+/**
+ *
+ *
+ */
+class MS_DynamicDates {
+
+	/**
+	 * Plugin version
+	 *
+	 * @since   1.0.0
+	 *
+	 * @var     string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 *
+	 * Unique identifier for plugin.
+	 *
+	 * The variable name is used as the text domain when internationalizing strings
+	 * of text. Its value should match the Text Domain file header in the main
+	 * plugin file.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	protected $plugin_slug = 'MS_DynamicDates';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      object
+	 */
+	protected static $instance = null;
+
+
+	/**
+	 * Initialize the plugin by setting localization and loading public scripts
+	 * and styles.
+	 *
+	 * @since     1.0.0
+	 */
+	private function __construct() {
+		$this->install_shortcodes();
+		add_filter('widget_text', 'do_shortcode');
+	}
+
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return    Plugin slug variable.
+	 */
+	public function get_plugin_slug() {
+		return $this->plugin_slug;
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    boolean    $network_wide    True if WPMU superadmin uses
+	 *                                       "Network Activate" action, false if
+	 *                                       WPMU is disabled or plugin is
+	 *                                       activated on an individual blog.
+	 */
+	public static function activate( $network_wide ) {
+
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+
+			if ( $network_wide  ) {
+
+				// Get all blog ids
+				$blog_ids = self::get_blog_ids();
+
+				foreach ( $blog_ids as $blog_id ) {
+					switch_to_blog( $blog_id );
+					self::single_activate();
+				}
+
+				restore_current_blog();
+
+			} else {
+				self::single_activate();
+			}
+
+		} else {
+			self::single_activate();
+		}
+
+	}
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    boolean    $network_wide    True if WPMU superadmin uses
+	 *                                       "Network Deactivate" action, false if
+	 *                                       WPMU is disabled or plugin is
+	 *                                       deactivated on an individual blog.
+	 */
+	public static function deactivate( $network_wide ) {
+
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+
+			if ( $network_wide ) {
+
+				// Get all blog ids
+				$blog_ids = self::get_blog_ids();
+
+				foreach ( $blog_ids as $blog_id ) {
+
+					switch_to_blog( $blog_id );
+					self::single_deactivate();
+
+				}
+
+				restore_current_blog();
+
+			} else {
+				self::single_deactivate();
+			}
+
+		} else {
+			self::single_deactivate();
+		}
+
+	}
+
+	/**
+	 * Fired when a new site is activated with a WPMU environment.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    int    $blog_id    ID of the new blog.
+	 */
+	public function activate_new_site( $blog_id ) {
+
+		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+			return;
+		}
+
+		switch_to_blog( $blog_id );
+		self::single_activate();
+		restore_current_blog();
+
+	}
+
+	/**
+	 * Get all blog ids of blogs in the current network that are:
+	 * - not archived
+	 * - not spam
+	 * - not deleted
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return   array|false    The blog ids, false if no matches.
+	 */
+	private static function get_blog_ids() {
+
+		global $wpdb;
+
+		// get an array of blog ids
+		$sql = "SELECT blog_id FROM $wpdb->blogs
+			WHERE archived = '0' AND spam = '0'
+			AND deleted = '0'";
+
+		return $wpdb->get_col( $sql );
+
+	}
+
+	/**
+	 * Fired for each blog when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 */
+	private static function single_activate() {
+
+		return true;
+	}
+
+	/**
+	 * Fired for each blog when the plugin is deactivated.
+	 *
+	 * @since    1.0.0
+	 */
+	private static function single_deactivate() {
+
+		return true;
+	}
+
+
+	/**
+	 * Add shortcodes
+	 *
+	 * @since    1.0.0
+	 */
+	public function install_shortcodes()
+	{
+
+		add_shortcode( 'dynamic_date', array(&$this, 'dynamic_date') );
+	}
+
+
+
+	/**
+	 * Implementation of dynamic date
+	 *
+	 * @since    1.0.0
+	 */
+	function dynamic_date($attr, $content)
+	{
+		$atts = shortcode_atts( array(
+			'days_before' => 3,
+			'format' => 'F d, Y'
+		), $atts );
+
+		if( is_numeric($atts['days_before']) ) {
+			return date($atts['format'], strtotime("-".$atts['days_before']." days"));
+		}
+		else
+			return date($atts['format']);
+	}
+
+}
+
+register_activation_hook( __FILE__, array( 'MS_DynamicDates', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'MS_DynamicDates', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'MS_DynamicDates', 'get_instance' ) );
